9a942baa1c985481799e60e3dd025e1c3b69dda9
sora <h.kamesaki@emotion-tech.co.jp>
2022-10-20 13:18:18 +0900

    エラー対応

--- lib/digdag_client/request.rb
+++ lib/digdag_client/request.rb
@@ -22,12 +22,12 @@ module Digdag
         path = "/api/#{path}"
         case method
         when :get, :delete
-          request.url(WEBrick::HTTPUtils.escape(path), options)
+          request.url(path, options)
         when :post, :put
           if method == :post
             request.headers['Content-Type'] = 'application/json'
           end
-          request.path = WEBrick::HTTPUtils.escape(path)
+          request.path = path
           request.body = options unless options.empty?
         end
       end
